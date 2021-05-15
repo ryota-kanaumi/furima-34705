3d5abd8c7e467d6002722dbb3383b5ec838ece00
ryota-kanaumi <ayusan.0729@icloud.com>
2021-05-15 18:18:25 +0900

    ログイン・ログアウト機能の実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-
-  devise_scope :users do
-    get '/users', to: redirect("/users/sign_up")
-  end
+  root to: 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'layouts#application'
+  resources :products 
 end
