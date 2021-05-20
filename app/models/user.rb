844418e00b2474f82d6a8812ba215e5d85dc21f4
ryota-kanaumi <ayusan.0729@icloud.com>
2021-05-20 12:34:52 +0900

    ユーザーモデルの単体テストコード実装①
    ユーザーモデルの単体テストコードの実装、ユーザーの新規登録がうまく行かないときの実装が完了。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          validates :nickname, presence: true
          validates :birthday, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :last_read_name, presence: true
-         validates :first_read_name, presence: true
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{6,32}+\z/
+         validates :password, presence: true, length: { minimum: 6, maximum: 32}, format: { with: VALID_PASSWORD_REGEX}
+         VALID_LAST_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+         validates :last_name, presence: true, format: { with: VALID_LAST_NAME_REGEX}
+         VALID_FIRST_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+         validates :first_name, presence: true, format: { with: VALID_FIRST_NAME_REGEX}
+         VALID_LAST_READ_NAME_REGEX = /\A[ァ-ヶー－]+\z/
+         validates :last_read_name, presence: true, format: { with: VALID_LAST_READ_NAME_REGEX}
+         VALID_FIRST_READ_NAME_REGEX = /\A[ァ-ヶー－]+\z/
+         validates :first_read_name, presence: true, format: { with: VALID_FIRST_READ_NAME_REGEX}
 end
