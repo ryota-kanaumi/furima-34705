fdd1cf2b7b3c2e26519e75910c1186b282687303
ryota-kanaumi <ayusan.0729@icloud.com>
2021-05-21 14:19:50 +0900

    バリデーションを一括にする記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         validates :nickname, presence: true
-         validates :birthday, presence: true
-         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{6,32}+\z/
-         validates :password, presence: true, length: { minimum: 6, maximum: 32}, format: { with: VALID_PASSWORD_REGEX}
-         VALID_LAST_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
-         validates :last_name, presence: true, format: { with: VALID_LAST_NAME_REGEX}
-         VALID_FIRST_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
-         validates :first_name, presence: true, format: { with: VALID_FIRST_NAME_REGEX}
-         VALID_LAST_READ_NAME_REGEX = /\A[ァ-ヶー－]+\z/
-         validates :last_read_name, presence: true, format: { with: VALID_LAST_READ_NAME_REGEX}
-         VALID_FIRST_READ_NAME_REGEX = /\A[ァ-ヶー－]+\z/
-         validates :first_read_name, presence: true, format: { with: VALID_FIRST_READ_NAME_REGEX}
+         with_options presence: true do
+          validates :nickname
+          validates :birthday
+          validates :password
+          validates :last_name
+          validates :first_name
+          validates :last_read_name
+          validates :first_read_name
+        end
+
+          VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{6,32}+\z/
+          validates :password, length: { minimum: 6, maximum: 32}, format: { with: VALID_PASSWORD_REGEX}
+          VALID_LAST_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+          validates :last_name, format: { with: VALID_LAST_NAME_REGEX}
+          VALID_FIRST_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+          validates :first_name, format: { with: VALID_FIRST_NAME_REGEX}
+          VALID_LAST_READ_NAME_REGEX = /\A[ァ-ヶー－]+\z/
+          validates :last_read_name, format: { with: VALID_LAST_READ_NAME_REGEX}
+          VALID_FIRST_READ_NAME_REGEX = /\A[ァ-ヶー－]+\z/
+          validates :first_read_name, format: { with: VALID_FIRST_READ_NAME_REGEX}
 end
