d6d951adbd2f6ca99c148ea5a04cfc10d9b30d94
ryota-kanaumi <ayusan.0729@icloud.com>
2021-05-26 16:44:23 +0900

    acdtive hashを用いた実装
    アクティブハッシュを用いてカテゴリなどをプルダウンの選択式にし、実装した。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,40 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    @product = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(product_params)
+    @product.save
+    if @product.save
+      redirect_to root_path
+    else
+      render action: :edit
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    unless @product.user == current_user
+      redirect_to root_path
+    end
+  end
+
+  def show
+
+  end
+
+  private
+
+  def product_params
+    binding.pry
+    params.require(:product).permit(:image, :product_name, :product_description, :category_id, :product_condition_id, :delivery_charge_id, :shipping_area_id, :delivery_day_id, :price)
+  end
+
 end
