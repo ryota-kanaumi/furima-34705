6a4317d5a7295a1150dff56475858e3731c4b47e
ryota-kanaumi <ayusan.0729@icloud.com>
2021-06-05 15:50:21 +0900

    モデルの記載とルーティングの記載

--- app/models/orders.rb
+++ app/models/orders.rb
@@ -1,7 +1,7 @@
 class Order Shipping Address
 
   include ActiveModel::Model
-  attr_accessor :
+  attr_accessor :token, :postal_code, :prefecture, :municipality, :address, :building_name, :phone_number, :purchase_record_id, :product_id, :user_id
   with_options presence: true do
     validates :token
     validates :postal_code, numericality: { with: /\A\d{3}[-]\d{4}\z/, message: 'Postal code is invalid. Enter it as follows (e.g. 123-4567)'}
@@ -11,7 +11,6 @@ class Order Shipping Address
     validatable :phone_number numericality:{ with: /\A\d{11}\z/}
   end
   
-
   def save
     
   end
