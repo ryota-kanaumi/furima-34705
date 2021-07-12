1b0796fb5fffaf74c15cb81d7eb242a43af113b3
ryota-kanaumi <ayusan.0729@icloud.com>
2021-07-12 13:23:12 +0900

    複数画像の投稿機能を実装

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
     belongs_to :user, optional: true
-    has_one_attached :image
+    has_many_attached :images
     has_one :purchase_record 
     
     #activehashの導入
