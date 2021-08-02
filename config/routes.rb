707971af092d96e5bebee66e2d52cd7438aa231d
ryota-kanaumi <ayusan.0729@icloud.com>
2021-08-02 18:15:21 +0900

    検索フォームの実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :products do
+    collection do
+      get 'search'
+    end
   resources :orders, only: [:index, :create]
   end
 end
